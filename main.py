04be09076f2522d7dc69cf1ed5c68ac1aeeb5fcb
RinoaTakako <steff0109@gmail.com>
2025-01-19 12:48:08 +0000

    2nd

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+contact_book = {}
 
-def contact_add(contact_book, name, email, phone):
-  if name in contact_book:
-    contact_book[name].update({'Email': email, 'Phone': phone})
+def contact_add(dictionary, name, email, phone):
+  if name in dictionary:
+    dictionary[name].update({'Email': email, 'Phone': phone})
   else:
-    contact_book[name] = {'Email': email, 'Phone': phone}
-    return contact_book
+    dictionary[name] = {'Email': email, 'Phone': phone}
+    return dictionary
 
-contact_book = {'Jen Boyle', 'jen1981@gmail.com', '076859473'}
 
 contact_add(contact_book, 'Steff Boyle', 'steff0109@gmail.com', '07742627852')
+contact_add(contact_book, 'John Boyle', '07547672565', 'john.hazukisan@gmail.com')
+contact_add(contact_book, 'Steff Boyle', 'rinoatakako@gmail.com', '07742627852')
 print(contact_book)
