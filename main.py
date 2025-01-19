44876e5b69c8ff5a4ea42b0630e9c7c7093c98b2
RinoaTakako <steff0109@gmail.com>
2025-01-19 14:06:16 +0000

    4th

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 # Starting dictionary
+#contact_book = [{'name': 'Jen Boyle', 'email':'jen1981@gmail.com', 'phone': '07567895343'}, {'name': 'Steff Boyle', 'email': 'steff0109@gmail.com', 'phone': '07742627852'}, {'name': 'John Boyle', 'email': 'john.hazukisan@gmail.com', 'phone': '07547672565'}]
+
 contact_book = {'name': 'Jen Boyle', 'email':'jen1981@gmail.com', 'phone': '07567895343'}
 
+def contact_menu(dictionary):
+  menu_input = input('Do you want to check or update your contact book? Check/Update ')
+  if menu_input == 'Check':
+    check_name = input('What is the contact you want to check? ')
+    contact_book_check(dictionary, check_name)
+  elif menu_input == 'Update':
+    contact_name = input('What is the contact you want to add or update? ')
+    contact_email = input('What is the email address of the contact? ')
+    contact_phone = input('What is the phone number of the contact? ')
+
+    contact_add_update(dictionary, contact_name, contact_email, contact_phone)
+  else:
+    print('Invalid Input. Please answer Check or Update')
+
+    
+    
 # Function to add or update dictionary
 def contact_add_update(dictionary, name, email, phone):
   if name in dictionary:
-    dictionary[name].update({'Email': email, 'Phone': phone})
+    print(f'The contact {name} is already in the contact book. ')
+    confirm = input('Do you wish to update? Y/N ')
+    if confirm == 'Y':
+        dictionary[name].update({'Email': email, 'Phone': phone})
+        print(f'The contact {name} has been updated')
+    else: 
+        print(f'The contact {name} has not been updated')
   else:
     dictionary[name] = {'Email': email, 'Phone': phone}
-    return dictionary
+    print(f'The contact {name} has been added')
 
-# Add to contact book
-contact_add_update(contact_book, 'Steff Boyle', 'steff0109@gmail.com', '07742627852')
-contact_add_update(contact_book, 'John Boyle', '07547672565', 'john.hazukisan@gmail.com')
-contact_add_update(contact_book, 'Steff Boyle', 'rinoatakako@gmail.com', '07742627852')
 
 # Check and print details from contact book
 def contact_book_check(dictionary, name):
@@ -21,7 +41,9 @@ def contact_book_check(dictionary, name):
     print(f'The contact is {name}', dictionary[name])
   else:
     print('Contact is not in contact book')
-    return dictionary
+    
+contact_menu(contact_book)
 
-contact_book_check(contact_book, 'Steff Boyle')
+print(contact_book)
+
 
